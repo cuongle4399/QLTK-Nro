43a3f6aa3be4bac04a1e3f50f44635b6092f513c
Cường Lê <161845711+cuongle4399@users.noreply.github.com>
2025-05-30 21:32:02 +0700

    fix loi van bay

--- QLTK Nro Pro/obj/Debug/net8.0-windows/QLTK Nro Pro.AssemblyInfo.cs
+++ QLTK Nro Pro/obj/Debug/net8.0-windows/QLTK Nro Pro.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cuong Le")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9277713ec809592d947ed274779fe5d0807d0e67")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e5bc582649888e1549e0bab08624b8ccd69498b")]
 [assembly: System.Reflection.AssemblyProductAttribute("QLTK Nro Pro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QLTK Nro Pro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
